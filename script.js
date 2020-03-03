013377cb325162fe8a5505ced3a0f7720ae10839
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-03-02 19:17:58 -0600

    unknown

--- script.js
+++ script.js
@@ -38,29 +38,44 @@ function hGenPlus() {
   addH(hPerSecond);
   update();
 }
-function buyHgen() {
-  if (hydrogen >= hGenCost) {
-    hydrogen = hydrogen - hGenCost;
-    hGen1++;
-    hPerSecond++;
-    hGenCost = Math.round((hGenCost * 2 + 10) / 1.5);
-    document.getElementById("HperSecond").innerHTML = hPerSecond + " H/s";
-    document.getElementById("hGenNum").innerHTML = hGen1;
-    document.getElementById("hGenCost").innerHTML = hGenCost + " H";
-    document.getElementById("hGenTotal").innerHTML = "Total: " + hGen1 + " H/s";
-    update();
+function buyGen(elm, tier) {
+  if (elm == "h") {
+    if (tier == "1" && hydrogen >= hGenCost) {
+      hydrogen = hydrogen - hGenCost;
+      hGen1++;
+      hPerSecond++;
+      hGenCost = Math.round((hGenCost * 2 + 10) / 1.5);
+      document.getElementById("HperSecond").innerHTML = hPerSecond + " H/s";
+      document.getElementById("hGenNum").innerHTML = hGen1;
+      document.getElementById("hGenCost").innerHTML = hGenCost + " H";
+      document.getElementById("hGenTotal").innerHTML = "Total: " + hGen1 + " H/s";
+      update();
+    } else if (tier == 2 && hydrogen >= hGenIICost) {
+      hydrogen -= hGenIICost;
+      hGenIICost = Math.round((hGenIICost * 3) / 1.7 + 10);
+      hGenII += x;
+      hPerSecond += (x * 3);
+      document.getElementById("HperSecond").innerHTML = hPerSecond + " H/s";
+      document.getElementById("hGenIINum").innerHTML = hGenII;
+      document.getElementById("hGenIICost").innerHTML = hGenIICost + " H";
+      document.getElementById("hGenIITotal").innerHTML = "Total: " + hGenII * 3 + " H/s";
+      update();
+    }
   }
 }
-function buyHgenII(x) {
-  if (hydrogen >= (hGenIICost * x)) {
-    hydrogen = hydrogen - hGenIICost * x;
-    hGenIICost = Math.round((hGenIICost * 3) / 1.7 + 10);
-    hGenII = hGenII + x;
-    hPerSecond = hPerSecond + (x * 3);
-    document.getElementById("HperSecond").innerHTML = hPerSecond + " H/s";
-    document.getElementById("hGenIINum").innerHTML = hGenII;
-    document.getElementById("hGenIICost").innerHTML = hGenIICost + " H";
-    document.getElementById("hGenIITotal").innerHTML = "Total: " + hGenII * 3 + " H/s"
-    update();
-  }
+function save() {
+  let items = [];
+  items.push(hydrogen);
+  items.push(h2);
+  items.push(totalH);
+  items.push(hGen1);
+  items.push(h2Unlock);
+  items.push(h3Unlock);
+  items.push(hGenCost);
+  items.push(hGen1);
+  items.push(hGenII);
+  items.map(x => JSON.stringify(x));
+  items.map(x => btoa(x));
+  items.join("");
+  return items[0];
 }
