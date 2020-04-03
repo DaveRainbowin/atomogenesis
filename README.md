6a4fcbfec7a53c33559e201f6b0800e6d7b621af
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-04-03 16:45:42 -0500

    shrinking the size a little bit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# atomogenesis
+# chemo creator
+## What is this game about?
+This game is about chemistry.
+You gain atoms and form molecules. If you have enough, you can do reactions.
+This is more towards the people who like chemistry and games.
--- script.js
+++ script.js
@@ -9,11 +9,11 @@ var hPerSecond = 0;
 var h2Unlock = false;
 var h3Unlock = false;
 var chance = Math.random();
-var hGenSecond = setInterval (hGenPlus, 1000);
+var hGenSecond = setInterval(hGenPlus, 1000);
 var check = setInterval (check, 10);
 function check() {
   if (hGen1 > 0) {
-    document.getElementById("HgenT2").style.display = "block";
+    getTag("HgenT2").style.display = "block";
   }
 }
 function addH(x) {
@@ -32,7 +32,7 @@ function addH(x) {
   }  
 }
 function update() {
-  document.getElementById("hydrogenNum").innerHTML = hydrogen;
+  getTag("hydrogenNum").innerHTML = hydrogen;
 }
 function hGenPlus() {
   addH(hPerSecond);
@@ -45,41 +45,24 @@ function buyGen(elm, tier) {
       hGen1++;
       hPerSecond++;
       hGenCost = Math.round((hGenCost * 2 + 10) / 1.5);
-      document.getElementById("HperSecond").innerHTML = hPerSecond + " H/s";
-      document.getElementById("hGenNum").innerHTML = hGen1;
-      document.getElementById("hGenCost").innerHTML = hGenCost + " H";
-      document.getElementById("hGenTotal").innerHTML = "Total: " + hGen1 + " H/s";
+      getTag("HperSecond").innerHTML = hPerSecond + " H/s";
+      getTag("hGenNum").innerHTML = hGen1;
+      getTag("hGenCost").innerHTML = hGenCost + " H";
+      getTag("hGenTotal").innerHTML = "Total: " + hGen1 + " H/s";
       update();
     } else if (tier == 2 && hydrogen >= hGenIICost) {
       hydrogen -= hGenIICost;
       hGenIICost = Math.round((hGenIICost * 3) / 1.7 + 10);
       hGenII += x;
-      hPerSecond += (x * 3);
-      document.getElementById("HperSecond").innerHTML = hPerSecond + " H/s";
-      document.getElementById("hGenIINum").innerHTML = hGenII;
-      document.getElementById("hGenIICost").innerHTML = hGenIICost + " H";
-      document.getElementById("hGenIITotal").innerHTML = "Total: " + hGenII * 3 + " H/s";
+      hPerSecond += x * 3;
+      getTag("HperSecond").innerHTML = hPerSecond + " H/s";
+      getTag("hGenIINum").innerHTML = hGenII;
+      getTag("hGenIICost").innerHTML = hGenIICost + " H";
+      getTag("hGenIITotal").innerHTML = "Total: " + hGenII * 3 + " H/s";
       update();
     }
   }
 }
-
-function save() {
-  let items = [];
-  items.push(hydrogen);
-  items.push(h2);
-  items.push(totalH);
-  items.push(hGen1);
-  items.push(h2Unlock);
-  items.push(h3Unlock);
-  items.push(hGenCost);
-  items.push(hGen1);
-  items.push(hGenII);
-  items.map(x => JSON.stringify(x));
-  items.map(x => btoa(x));
-  items.join("");
-  return items[0];
-}
-function newGen(tier, elm, cost) {
-  
+function getTag(id) {
+  return document.getElementById(id);
 }
