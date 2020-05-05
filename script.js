4fa0056fe8edd888dfb12f6e560af6ad26b65e61
DaveRainbowin <51142390+DaveRainbowin@users.noreply.github.com>
2020-05-05 15:29:16 -0500

    tabs + less space i added 2 tabs i reduced the size of the script by a little

--- script.js
+++ script.js
@@ -9,14 +9,27 @@ var hGen3 = 0;
 var hGenCost3 = 250;
 var hGen4 = 0;
 var hGenCost4 = 1000;
+var c = 0;
+var cPerSecond = 0;
+var totalC = 0;
+var cGen1 = 0;
+var cGenCost1 = 15;
+var cGen2 = 0;
+var cGenCost2 = 100;
+var cGen3 = 0;
+var cGenCost3 = 250;
+var cGen4 = 0;
+var cGenCost4 = 1000;
+var h2 = 0;
+var ch4 = 0;
 setInterval(addH, 1000);
 function addH() {
   h += hPerSecond;
   update();
 }
 function update() {
-  getTag("hydrogenNum").innerHTML = h;
-  getTag("HperSecond").innerHTML = `${hPerSecond}  H/s`;
+  get("hydrogenNum").innerHTML = h;
+  get("HperSecond").innerHTML = `${hPerSecond}  H/s`;
 }
 function buyGen(elm, tier) {
   if (elm == "h") {
@@ -25,44 +38,43 @@ function buyGen(elm, tier) {
       hGen1++;
       hPerSecond++;
       hGenCost1 = Math.round((hGenCost1 * 2 + 10) / 1.5);
-      getTag("hGenNum1").innerHTML = `${hGen1}`;
-      getTag("hGenTotal1").innerHTML = `Total: ${hGen1} H/s`;
-      getTag("hGenCost1").innerHTML = `${hGenCost1} H`;
-      getTag("HgenT2").style.display = "block";
+      get("hGenNum1").innerHTML = `${hGen1}`;
+      get("hGenTotal1").innerHTML = `Total: ${hGen1} H/s`;
+      get("hGenCost1").innerHTML = `${hGenCost1} H`;
+      get("HgenT2").style.display = "block";
       update();
     } else if (tier == 2  && h >= hGenCost2) {
       h -= hGenCost2;
       hGen2++;
       hPerSecond += 3;
       hGenCost2 = Math.round((hGenCost2 * 2 + 10) / 1.5);
-      getTag("hGenNum2").innerHTML = `${hGen2}`;
-      getTag("hGenTotal2").innerHTML = `Total: ${hGen2 * 3} H/s`;
-      getTag("hGenCost2").innerHTML = `${hGenCost2} H`;
-      getTag("HgenT3").style.display = "block";
+      get("hGenNum2").innerHTML = `${hGen2}`;
+      get("hGenTotal2").innerHTML = `Total: ${hGen2 * 3} H/s`;
+      get("hGenCost2").innerHTML = `${hGenCost2} H`;
+      get("HgenT3").style.display = "block";
       update();
     } else if (tier == 3 && h >= hGenCost3) {
       h -= hGenCost3;
       hGen3++;
       hPerSecond += 10;
       hGenCost3 = Math.round((hGenCost3 * 2 + 10) / 1.5);
-      getTag("hGenNum3").innerHTML = `${hGen3}`;
-      getTag("hGenTotal3").innerHTML = `Total: ${hGen3 * 10} H/s`;
-      getTag("hGenCost3").innerHTML = `${hGenCost3} H`;
-      getTag("HgenT4").style.display = "block";
+      get("hGenNum3").innerHTML = `${hGen3}`;
+      get("hGenTotal3").innerHTML = `Total: ${hGen3 * 10} H/s`;
+      get("hGenCost3").innerHTML = `${hGenCost3} H`;
+      get("HgenT4").style.display = "block";
       update();
     } else if (tier == 4 && h >= hGenCost4) {
       h -= hGenCost4;
       hGen4++;
       hPerSecond += 50;
       hGenCost4 = Math.round((hGenCost4 * 2 + 10) / 1.5);
-      getTag("hGenNum4").innerHTML = `${hGen4}`;
-      getTag("hGenTotal4").innerHTML = `Total: ${hGen4 * 50} H/s`;
-      getTag("hGenCost4").innerHTML = `${hGenCost4} H`;
+      get("hGenNum4").innerHTML = `${hGen4}`;
+      get("hGenTotal4").innerHTML = `Total: ${hGen4 * 50} H/s`;
+      get("hGenCost4").innerHTML = `${hGenCost4} H`;
       update();
     }
   }
 }
-
-function getTag(id) {
+function get(id) {
   return document.getElementById(id);
 }
